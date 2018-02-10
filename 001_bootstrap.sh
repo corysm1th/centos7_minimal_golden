89dce9d9d5625bd5581fc43aa7ba9b637c5e5667
Cory Smith <smith.cory@gmail.com>
2018-02-10 13:04:25 -0800

    reboots should be handled elsewhere

--- 001_bootstrap.sh
+++ 001_bootstrap.sh
@@ -7,9 +7,7 @@ yum update -y
 yum install -y ansible bind-utils certbot git htop iftop iptables-services net-tools tcpdump wget yum-utils
 
 curl https://raw.githubusercontent.com/corysm1th/centos7_minimal_golden/master/runonce_setup.sh | sh
-mkdir .ssh
-touch .ssh/authorized_keys
-chmod 700 .ssh
-chmod 600 .ssh/authorized_keys
-
-shutdown -r now
+mkdir /root/.ssh
+touch /root/.ssh/authorized_keys
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
